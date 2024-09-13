c39461914b1ec40b1ada04961ec7059e7b4d590b
AchilesCS <152240777+AchilesCS@users.noreply.github.com>
2024-09-14 00:51:15 +0800

    Add files via upload

---
+++ jvs for portfolio.js
@@ -0,0 +1,11 @@
+document.getElementById('hireMeButton').addEventListener('click', function() {
+    document.getElementById('contact-section').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('experienceButton').addEventListener('click', function() {
+    document.getElementById('about-section').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
